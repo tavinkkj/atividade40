d2ed6385a1e501d511d221c20276504f8b16d7ab
Otávio <otavio.olivindo@aluno.ce.gov.br>
2025-10-16 13:39:05 -0300

    Add files via upload

---
+++ atv40.py
@@ -0,0 +1,6 @@
+# Contagem de Usuários no Sistema
+# Faça um programa que conte de 1 até 10, mostrando na tela a mensagem “Usuário X verificado com sucesso”.
+i = 1
+
+for i in range(1, 11):
+    print(f'Usuário {i} verificado com sucesso')
